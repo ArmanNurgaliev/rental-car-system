8b83f228d5cd3e5afcf231bc0570e81b7b41df57
Arman Nurgaliev <armanurgaliev@gmail.com>
2024-05-20 15:29:46 +0700

    Fixed mistakes

--- api-gateway/src/main/java/ru/arman/config/GatewayConfig.java
+++ api-gateway/src/main/java/ru/arman/config/GatewayConfig.java
@@ -17,9 +17,8 @@ public class GatewayConfig {
         return builder.routes()
                 .route("auth-service", r -> r.path("/api/user/**").filters(f -> f.filter(filter)).uri("lb://auth-service"))
                 .route("location-service", r -> r.path("/api/location/**").filters(f -> f.filter(filter)).uri("lb://location-service"))
-                .route("location-service", r -> r.path("/api/reservation/**").filters(f -> f.filter(filter)).uri("lb://reservation-service"))
-//                .route("payment-service", r -> r.path("/api/payment/**").filters(f -> f.filter(filter)).uri("lb://payment-service"))
-                .route("location-service", r -> r.path("/api/vehicle/**").filters(f -> f.filter(filter)).uri("lb://vehicle-service"))
+                .route("reservation-service", r -> r.path("/api/reservation/**").filters(f -> f.filter(filter)).uri("lb://reservation-service"))
+                .route("vehicle-service", r -> r.path("/api/vehicle/**").filters(f -> f.filter(filter)).uri("lb://vehicle-service"))
                 .route("discovery-service", r -> r.path("/eureka/web").filters(f -> f.setPath("/")).uri("http://localhost:8761"))
                 .route("discovery-service-static", r -> r.path("/eureka/**").uri("http://localhost:8761"))
                 .build();
--- notification-service/src/main/java/ru/arman/service/NotificationService.java
+++ notification-service/src/main/java/ru/arman/service/NotificationService.java
@@ -2,13 +2,9 @@ package ru.arman.service;
 
 import jakarta.mail.Message;
 import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSendException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
